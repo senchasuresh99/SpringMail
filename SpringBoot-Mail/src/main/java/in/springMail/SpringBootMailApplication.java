4849e05cf583793e45631b402f5790219e383c2c
Suresh <senchasuresh99@gmail.com>
2023-05-10 15:51:33 +0530

    commit by Suresh Sencha

---
+++ SpringBoot-Mail/src/main/java/in/springMail/SpringBootMailApplication.java
@@ -0,0 +1,27 @@
+package in.springMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.springMail.service.IPurchaseOrder;
+
+@SpringBootApplication
+public class SpringBootMailApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringBootMailApplication.class, args);
+		IPurchaseOrder order = context.getBean(IPurchaseOrder.class);
+		try {
+			String msg = order.purchase(new String[] { "fossil ", "t-shirt", "shoes" },
+					new double[] { 2345.98, 5647.09, 897.09 }, new String[] { "senchasuresh44@gmail.com",
+							"seervisencha44@gmail.com", "senchasuresh99@gmail.com" });
+			System.out.println(msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		((ConfigurableApplicationContext) context).close();
+	}
+
+}
